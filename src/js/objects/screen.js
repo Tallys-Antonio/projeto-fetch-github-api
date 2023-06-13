d0422c31a91d89313f7ba913de634de9a61151fd
Tallys Antonio <tallysantonio@gmail.com>
2023-06-13 16:38:47 -0300

    adicionando novas funcionalidades pedidas pelo DevQuest

--- src/js/objects/screen.js
+++ src/js/objects/screen.js
@@ -6,20 +6,59 @@ const screen = {
                             <div class="data">
                                 <h1>${user.name ?? 'Esse perfil não possui nome cadastrado 😓'}</h1>
                                 <p>${user.bio ?? 'Esse perfil não possui bio cadastrada 😓'}</p>
+                                <div class="follow">
+                                    <div class="followers">
+                                        <h3>👥Seguidores:</h3>
+                                        <p>${user.followers}</p>
+                                    </div>
+                                    <div class="following">
+                                        <h3>👤Seguindo:</h3>
+                                        <p>${user.following}</p>
+                                    </div>
+                                </div>
                             </div>
                         </div>`
-        
-        let repositoriesItens = ''
-        user.repositories.forEach(repo => repositoriesItens += `<li><a href="${repo.html_url}" target="_blank">${repo.name}</a></li>`)
 
-        if(user.repositories.length > 0){
+        let repositoriesItens = ''
+        user.repositories.forEach(repo => repositoriesItens += `<li><a href="${repo.html_url}" target="_blank">
+                            <h4>${repo.name}</h4>
+                            <div class="repo-info">
+                                <p>🍴 ${repo.forks}</p>
+                                <p>⭐ ${repo.stargazers_count}</p>
+                                <p>👀 ${repo.watchers}</p>
+                                <p>👨‍💻 ${repo.language}</p>
+                            </div>
+                        </a></li>`)
+
+        if (user.repositories.length > 0) {
             this.userProfile.innerHTML += `<div class="repositories section">
                                                 <h2>Repositórios</h2>
                                                 <ul>${repositoriesItens}</ul>
                                             </div>`
         }
+
+        let eventsItens = '';
+        user.events.forEach(evento => {
+            if (evento.type === 'PushEvent') {
+                eventsItens += `<li>
+                                    <h3>${evento.repo.name} </h3>
+                                    <p> - ${evento.payload.commits[0].message}</p>
+                                </li>`
+            } else {
+                eventsItens += `<li>
+                                    <h3>${evento.repo.name} </h3>
+                                    <p> - Criado um ${evento.payload.ref_type}</p>
+                                </li>`
+            }
+        })
+        if (user.events.length > 0) {
+            this.userProfile.innerHTML += `<div class="events">
+                                                <h2>Eventos:</h2>
+                                                <ul>${eventsItens}</ul>
+                                            </div>`
+        }
     },
-    renderNotFound(){
+    renderNotFound() {
         this.userProfile.innerHTML = "<h3>Usuário não encontrado😥</h3>"
     }
 }
--- src/js/objects/user.js
+++ src/js/objects/user.js
@@ -3,15 +3,27 @@ const user = {
     name: '',
     bio: '',
     userName: '',
+    followers: '',
+    following: '',
     repositories: [],
-    setInfo(gitHubUser){
+    events: [],
+    setInfo(gitHubUser) {
         this.avatarUrl = gitHubUser.avatar_url
         this.name = gitHubUser.name
         this.bio = gitHubUser.bio
-        this.userName = gitHubUser.login 
+        this.userName = gitHubUser.login
+        this.followers = gitHubUser.followers
+        this.following = gitHubUser.following
     },
-    setRepositories(repositories){
+    setRepositories(repositories) {
         this.repositories = repositories
+    },
+    setEvents(eventsList) {
+        this.events = eventsList.filter(function (event) {
+            if (event.type === "PushEvent" || event.type === "CreateEvent") {
+                return event
+            }
+        })
     }
 }
 
